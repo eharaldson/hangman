2cfe226570b5f31e603388745723ed1da78e339a
Erik Haraldson <erikharaldson@MacBook-Pro-9.local>
2022-11-03 10:34:00 +0000

    Fixes issue with task 1

--- milestone_4.py
+++ milestone_4.py
@@ -1,20 +1,18 @@
 import random
 
-def num_unique(ls):
-    ls_set = set(ls)
-    return len(ls_set)
-
 class Hangman:
 
+    # Class constructor
     def __init__(self, word_list, num_lives=5):
 
         self.word = random.choice(word_list)                # Random word from the list to be guessed.
         self.word_guessed = ['_' for x in self.word]        # List of letters guessd. _ if not been guessed yet.
-        self.num_letters = num_unique(self.word)            # Number of unique letters in the word to be guessed.
+        self.num_letters = len(set(self.word))              # Number of unique letters in the word to be guessed.
         self.num_lives = num_lives                          # Number of lives left.
         self.word_list = word_list                          # List of words to pick from.
         self.list_of_guesses = []                           # Letters that have been guessed by player.
 
+    # Methods
     def check_guess(self, guess):
         guess.lower()
         if guess in self.word:
